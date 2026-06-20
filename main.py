387e980eb26367ad1760b2faeaff830b52c793e1
abdullamansor2020-wq <abdulla.mansor2020@gmail.com>
2026-06-20 15:17:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,25 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.get("/")
 def root():
-    return {
-        "message": "NWC Tabuk SafeSite API Running"
-    }
+    return {"message": "NWC Tabuk SafeSite API Running"}
 
 @app.post("/analyze")
 async def analyze(file: UploadFile = File(...)):
-    file_path = f"{UPLOAD_DIR}/{file.filename}"
+    try:
+        file_path = os.path.join(UPLOAD_DIR, file.filename)
 
-    with open(file_path, "wb") as buffer:
-        shutil.copyfileobj(file.file, buffer)
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
 
-    result = analyze_image(file_path)
-    return result
+        result = analyze_image(file_path)
+
+        return {
+            "status": "success",
+            "result": result
+        }
+
+    except Exception as e:
+        return {
+            "status": "error",
+            "message": str(e)
+        }
